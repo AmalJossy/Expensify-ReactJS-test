d9433d7d86f04d70b9533a30450fb5a19f19edd1
Amal Jossy <amaljossy1@gmail.com>
2018-01-22 17:39:12 +0530

    undo last commit, fixed heroku fail(typo)

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import getVisibleExpenses from './../selectors/expense';
+import getVisibleExpenses from '../selectors/expense';
 import ExpenseListItem from './ExpenseListItem';
 const ExpenseList = (props) => (
     <div>
